3efac8111a244b5bb7d4eb119dd0279f4546f25a
WarZicks <36964591+WarZicks@users.noreply.github.com>
2018-09-24 14:15:44 +0200

    player_spawn

---
+++ BR_Project02/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public string MonTexte;
+
+    public static GameManager Singleton;
+
+    void Awake ()
+    {
+        if (Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Singleton = this;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AfficheMonTexte ()
+    {
+        Debug.Log(MonTexte);
+    }
+}
