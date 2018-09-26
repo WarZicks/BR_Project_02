030e6f4639223ee59c8c03ebab50975137ea1852
size45 <33767630+size45@users.noreply.github.com>
2018-09-26 14:28:54 +0200

    Main_Menu

---
+++ BR_Project02/Assets/Scripts/Ui_Manager_MM.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Ui_Manager_MM : MonoBehaviour {
+    public GameObject Jouer;
+    public GameObject Quitter;
+    public GameObject Network;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnClick()
+    {
+        Jouer.SetActive(false);
+        Quitter.SetActive(false);
+        Network.SetActive(true);
+    }
+
+    public void OnclickExit()
+    {
+        Application.Quit();
+    }
+}
